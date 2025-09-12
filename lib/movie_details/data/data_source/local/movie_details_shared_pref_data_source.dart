a778c3459798231337a22736ef9fbd73a47d0ad4
Omar Adel <3maradel010@gmail.com>
2025-09-12 16:10:35 +0300

    initialize movie details screen folder

---
+++ lib/movie_details/data/data_source/local/movie_details_shared_pref_data_source.dart
@@ -0,0 +1,3 @@
+import 'package:movies/movie_details/data/data_source/local/movie_details_local_data_source.dart';
+
+class MovieDetailsSharedPrefDataSource implements MovieDetailsLocalDataSource {}
---
+++ lib/movie_details/view/screens/movie_details_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieDetailsScreen extends StatefulWidget {
+  const MovieDetailsScreen({super.key});
+
+  @override
+  State<MovieDetailsScreen> createState() => _MovieDetailsScreenState();
+}
+
+class _MovieDetailsScreenState extends State<MovieDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
