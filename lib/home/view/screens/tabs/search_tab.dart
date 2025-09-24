387c966ebb1124fd51e01f23f9a0610d0e474de8
Omar Adel <156123711+OmarAdel10@users.noreply.github.com>
2025-09-24 22:46:11 +0300

    fixed search tab paddding

--- lib/home/view/screens/tabs/search_tab.dart
+++ lib/home/view/screens/tabs/search_tab.dart
@@ -20,123 +20,126 @@ class _SearchTabState extends State<SearchTab> {
   int page = 1;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 50),
-        SearchField(
-          controller: controller,
-          onChanged: (value) {
-            context.read<MoviesSearchCubit>().searchMovies(value, page: page);
-          },
-        ),
-        // SizedBox(height: 13),
-
-        Expanded(
-          child: BlocBuilder<MoviesSearchCubit, SearchState>(
-            builder: (context, state) {
-              if (state is SearchLoading) {
-                return const Center(
-                  child: CircularProgressIndicator(color: AppTheme.yellow),
-                );
-              } else if (state is SearchError) {
-                return Center(child: Text(state.errorMessage));
-              } else if (state is SearchSuccess) {
-                final movies = state.movies;
-                log('movies length: ${movies.length}');
-                return GridView.builder(
-                  shrinkWrap: true,
-
-                  itemCount: movies.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.7,
-                    crossAxisSpacing: 12,
-                    mainAxisSpacing: 12,
-                  ),
-                  itemBuilder: (context, index) {
-                    final movie = movies[index];
-                    final imageUrl = movie.mediumCoverImage ?? '';
-
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.of(
-                          context,
-                        ).pushNamed('movie-details', arguments: movie.id);
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          color: Colors.grey[200],
-                        ),
-                        child: Stack(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(12),
-                              child: imageUrl.isNotEmpty
-                                  ? Image.network(
-                                      imageUrl,
-                                      width: double.infinity,
-                                      height: double.infinity,
-                                      fit: BoxFit.cover,
-                                    )
-                                  : Container(
-                                      color: Colors.grey[300],
-                                      child: const Center(
-                                        child: Icon(Icons.movie, size: 40),
-                                      ),
-                                    ),
-                            ),
-                            Positioned(
-                              top: 8,
-                              left: 8,
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 6,
-                                  vertical: 4,
-                                ),
-                                decoration: BoxDecoration(
-                                  color: Colors.black.withOpacity(0.6),
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    const Icon(
-                                      Icons.star,
-                                      color: Colors.yellow,
-                                      size: 14,
-                                    ),
-                                    const SizedBox(width: 4),
-                                    Text(
-                                      movie.rating?.toStringAsFixed(1) ?? 'N/A',
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                );
-              } else {
-                return const Center(
-                  child: Text(
-                    'No movies found',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                );
-              }
+    return SafeArea(
+      child: Column(
+        children: [
+          SearchField(
+            controller: controller,
+            onChanged: (value) {
+              context.read<MoviesSearchCubit>().searchMovies(value, page: page);
             },
           ),
-        ),
-      ],
+      
+          Expanded(
+            child: BlocBuilder<MoviesSearchCubit, SearchState>(
+              builder: (context, state) {
+                if (state is SearchLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator(color: AppTheme.yellow),
+                  );
+                } else if (state is SearchError) {
+                  return Center(child: Text(state.errorMessage));
+                } else if (state is SearchSuccess) {
+                  final movies = state.movies;
+                  log('movies length: ${movies.length}');
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: GridView.builder(
+                      shrinkWrap: true,
+                    
+                      itemCount: movies.length,
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 0.7,
+                        crossAxisSpacing: 12,
+                        mainAxisSpacing: 12,
+                      ),
+                      itemBuilder: (context, index) {
+                        final movie = movies[index];
+                        final imageUrl = movie.mediumCoverImage ?? '';
+                    
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.of(
+                              context,
+                            ).pushNamed('movie-details', arguments: movie.id);
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.grey[200],
+                            ),
+                            child: Stack(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(12),
+                                  child: imageUrl.isNotEmpty
+                                      ? Image.network(
+                                          imageUrl,
+                                          width: double.infinity,
+                                          height: double.infinity,
+                                          fit: BoxFit.cover,
+                                        )
+                                      : Container(
+                                          color: Colors.grey[300],
+                                          child: const Center(
+                                            child: Icon(Icons.movie, size: 40),
+                                          ),
+                                        ),
+                                ),
+                                Positioned(
+                                  top: 8,
+                                  left: 8,
+                                  child: Container(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 6,
+                                      vertical: 4,
+                                    ),
+                                    decoration: BoxDecoration(
+                                      color: Colors.black.withOpacity(0.6),
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    child: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        const Icon(
+                                          Icons.star,
+                                          color: Colors.yellow,
+                                          size: 14,
+                                        ),
+                                        const SizedBox(width: 4),
+                                        Text(
+                                          movie.rating?.toStringAsFixed(1) ?? 'N/A',
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                } else {
+                  return const Center(
+                    child: Text(
+                      'No movies found',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
